5b5565e7660bd578503db89b4b18c93f6a779d65
Mylena Assis <lena.assis.dev@gmail.com>
2024-06-03 06:15:23 -0300

    Added - busca linear

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+//busca linear em vetor nao ordenado
+int i = 1;
+int bl = -1;
+int x = 3; //declaracao do item buscado
+
+int[] numeros = { 5, 3, 8, 1, 2 };
+
+while (i < numeros.Length)
+{
+    if (numeros[i] == x)
+    {
+        bl = i;
+        i = numeros.Length + 1; //saindo do loop
+        Console.WriteLine($"Elemento encontrado no índice {bl}");
+    } else
+    {
+        i++; //busca continua
+    }
+}
